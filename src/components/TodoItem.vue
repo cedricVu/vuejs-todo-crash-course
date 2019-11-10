2748cf24f61c0339415d267ce09bb98743599cce
Son vu <ngocson.bkdn@gmail.com>
2019-11-10 18:06:45 +0700

    Vue crash course

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete" />
+            {{todo.title}}
+            <button @click="$emit('del-todo', todo.id)">x</button>
+        </p>
+    </div>
+</template>
+
+<script>
+    // import axios from 'axios';
+    export default {
+        name: "TodoItem",
+        props: ["todo"],
+        methods: {
+            markComplete() {
+                this.$emit("mark-complete", this.todo.id);
+                // this.todo.completed = !this.todo.completed;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .todo-item {
+        background: #fff4ff;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+    .is-complete {
+        text-decoration: line-through;
+    }
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" v-on:mark-complete="$emit('mark-complete', todo.id)" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import TodoItem from './TodoItem.vue'
+    export default {
+        name: 'todos',
+        props: ["todos"],
+        components: {
+            TodoItem
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
